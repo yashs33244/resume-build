2d50e947c3c260ae2d859f8eed09b742779da329
Vaibhav Agrawal <vaibhav.agrawal@cars24.com>
2024-09-14 23:02:33 +0530

    change templates css from scss to css

--- apps/docs/components/Editor/templates/template2.tsx
+++ apps/docs/components/Editor/templates/template2.tsx
@@ -2,67 +2,68 @@
 import React, { useState } from "react";
 //@ts-ignore
 import DOMPurify from 'dompurify';
-import styles from "./template2.module.scss";
+import  "./template2.css";
 
 
 export const Template2 = (props:any) => {
     const { resumeData, id } = props;
 
     return (
-        <div className={styles['template-container']} id={id}>
-            <div className={styles['header']}>
-                <div className={styles['header_left_part']}>
-                    <div className={styles['header_left_part_name']}>{resumeData?.personalInfo?.name}</div>
-                    <div className={styles['header_left_part_role']}>{resumeData?.personalInfo?.title}</div>
+      <div className="template2-wrapper">
+        <div className={'template-container'} id={id}>
+            <div className={'header'}>
+                <div className={'header_left_part'}>
+                    <div className={'header_left_part_name'}>{resumeData?.personalInfo?.name}</div>
+                    <div className={'header_left_part_role'}>{resumeData?.personalInfo?.title}</div>
                 </div>
-                <div className={styles['header_right_part']}>
-                    <div className={styles['header_right_part_url']}>{resumeData.personalInfo.email}</div>
-                    <div className={styles['header_right_part_phone']}>
+                <div className={'header_right_part'}>
+                    <div className={'header_right_part_url'}>{resumeData.personalInfo.email}</div>
+                    <div className={'header_right_part_phone'}>
                         <a href={resumeData.personalInfo?.linkedin}>{resumeData.personalInfo?.linkedin}</a>
                     </div>
-                    <div className={styles['header_right_part_email']}>{resumeData.personalInfo.phone}</div>
+                    <div className={'header_right_part_email'}>{resumeData.personalInfo.phone}</div>
                 </div>
             </div>
-            <div className={styles['content']}>
-                <div className={`${styles['content_bio_div']} ${styles['content_container']}`}>
-                    <div className={`${styles['content_bio_div_label']} ${styles['content_left_part']}`}>
+            <div className={'content'}>
+                <div className={`${'content_bio_div'} ${'content_container'}`}>
+                    <div className={`${'content_bio_div_label'} ${'content_left_part'}`}>
                         Bio
                     </div>
-                    <div className={`${styles['content_bio_div_value']} ${styles['content_right_part']}`}>
+                    <div className={`${'content_bio_div_value'} ${'content_right_part'}`}>
                         {resumeData.personalInfo.bio}
                     </div>
                 </div>
-                <div className={`${styles['content_education_div']} ${styles['content_container']}`}>
-                    <div className={`${styles['content_education_div_label']} ${styles['content_left_part']}`}>
+                <div className={`${'content_education_div'} ${'content_container'}`}>
+                    <div className={`${'content_education_div_label'} ${'content_left_part'}`}>
                         Education
                     </div>
-                    <div className={`${styles['content_education_div_value']} ${styles['content_right_part']}`}>
+                    <div className={`${'content_education_div_value'} ${'content_right_part'}`}>
                         {
                             resumeData.education?.map((edu:any) => {
                                 return(
-                                    <div className={styles['content_education_div_value_section']}>
-                                        <div className={styles['content_education_div_value_section_name']}>{edu?.institution}</div>
-                                        <div className={styles['content_education_div_value_section_year']}>{edu?.start + " - " + edu?.end}</div>
-                                        <div className={styles['content_education_div_value_section_field']}>{edu?.degree + " - " + edu?.major}</div>
+                                    <div className={'content_education_div_value_section'}>
+                                        <div className={'content_education_div_value_section_name'}>{edu?.institution}</div>
+                                        <div className={'content_education_div_value_section_year'}>{edu?.start + " - " + edu?.end}</div>
+                                        <div className={'content_education_div_value_section_field'}>{edu?.degree + " - " + edu?.major}</div>
                                     </div>
                                 )
                             })
                         }
                     </div>
                 </div>
-                <div className={`${styles['content_experience_div']} ${styles['content_container']}`}>
-                    <div className={`${styles['content_experience_div_label']} ${styles['content_left_part']}`}>
+                <div className={`${'content_experience_div'} ${'content_container'}`}>
+                    <div className={`${'content_experience_div_label'} ${'content_left_part'}`}>
                         Experience
                     </div>
-                    <div className={`${styles['content_experience_div_value']} ${styles['content_right_part']}`}>
+                    <div className={`${'content_experience_div_value'} ${'content_right_part'}`}>
                         {
                             resumeData.experience?.map((exp:any) => {
                                 return(
-                                    <div className={styles['content_experience_div_value_section']}>
-                                        <div className={styles['content_experience_div_value_section_name']}>{exp?.company + " - " + exp?.role}</div>
-                                        <div className={styles['content_experience_div_value_section_year']}>{exp?.duration || (exp?.start + " - " + exp.end)}</div>
+                                    <div className={'content_experience_div_value_section'}>
+                                        <div className={'content_experience_div_value_section_name'}>{exp?.company + " - " + exp?.role}</div>
+                                        <div className={'content_experience_div_value_section_year'}>{exp?.duration || (exp?.start + " - " + exp.end)}</div>
                                         <div 
-                                            className={styles['content_experience_div_value_section_work']}
+                                            className={'content_experience_div_value_section_work'}
                                             dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(exp.responsibilities) }}
                                         ></div>
                                     </div>
@@ -71,32 +72,32 @@ export const Template2 = (props:any) => {
                         }
                     </div>
                 </div>
-                <div className={`${styles['content_skill_div']} ${styles['content_container']}`}>
-                    <div className={`${styles['content_skill_div_label']} ${styles['content_left_part']}`}>
+                <div className={`${'content_skill_div'} ${'content_container'}`}>
+                    <div className={`${'content_skill_div_label'} ${'content_left_part'}`}>
                         Skill
                     </div>
-                    <div className={`${styles['content_skill_div_value']} ${styles['content_right_part']}`}>
-                        <div className={styles['content_skill_div_value_section']}>
-                            <div className={styles['content_skill_div_value_section_category']}>Core Skills</div>
-                            <div className={styles['content_skill_div_value_section_skills']}>{resumeData?.coreSkills?.join(', ')}</div>
+                    <div className={`${'content_skill_div_value'} ${'content_right_part'}`}>
+                        <div className={'content_skill_div_value_section'}>
+                            <div className={'content_skill_div_value_section_category'}>Core Skills</div>
+                            <div className={'content_skill_div_value_section_skills'}>{resumeData?.coreSkills?.join(', ')}</div>
                         </div>
-                        <div className={styles['content_skill_div_value_section']}>
-                            <div className={styles['content_skill_div_value_section_category']}>Tools and Technologies</div>
-                            <div className={styles['content_skill_div_value_section_skills']}>{resumeData?.techSkills?.join(', ')}</div>
+                        <div className={'content_skill_div_value_section'}>
+                            <div className={'content_skill_div_value_section_category'}>Tools and Technologies</div>
+                            <div className={'content_skill_div_value_section_skills'}>{resumeData?.techSkills?.join(', ')}</div>
                         </div>
                     </div>
                 </div>
-                <div className={`${styles['content_acheivement_div']} ${styles['content_container']}`}>
-                    <div className={`${styles['content_acheivement_div_label']} ${styles['content_left_part']}`}>
+                <div className={`${'content_acheivement_div'} ${'content_container'}`}>
+                    <div className={`${'content_acheivement_div_label'} ${'content_left_part'}`}>
                         Achievement
                     </div>
-                    <div className={`${styles['content_acheivement_div_value']} ${styles['content_right_part']}`}>
+                    <div className={`${'content_acheivement_div_value'} ${'content_right_part'}`}>
                         {
                             resumeData.certificate?.map((achievements:any) => {
                                 return(
-                                    <div className={styles['content_acheivement_div_value_section']}>
-                                        <div className={styles['content_acheivement_div_value_section_title']}>{achievements?.issuer + ' - ' + achievements?.issuedOn}</div>
-                                        <div className={styles['content_acheivement_div_value_section_remarks']}>{achievements?.name}</div>
+                                    <div className={'content_acheivement_div_value_section'}>
+                                        <div className={'content_acheivement_div_value_section_title'}>{achievements?.issuer + ' - ' + achievements?.issuedOn}</div>
+                                        <div className={'content_acheivement_div_value_section_remarks'}>{achievements?.name}</div>
                                     </div>
                                 )
                             })
@@ -105,5 +106,6 @@ export const Template2 = (props:any) => {
                 </div>
             </div>
         </div>
+      </div>
     );
 };
--- apps/docs/components/Editor/templates/template3.tsx
+++ apps/docs/components/Editor/templates/template3.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 //@ts-ignore
 import DOMPurify from 'dompurify';
-import styles from "./template3.module.scss";
+import "./template3.css";
 import { IoMail } from "react-icons/io5";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
@@ -11,87 +11,88 @@ export const Template3 = (props:any) => {
     const { resumeData, id } = props;
 
     return (
-        <div className={styles['template-container']} id={id}>
-            <div className={styles['lp']}>
-                <div className={styles['lp_personalInfo']}>
-                    <div className={styles['lp_personalInfo_name']}>{resumeData?.personalInfo?.name}</div>
-                    <div className={styles['lp_personalInfo_role']}>{resumeData?.personalInfo?.title}</div>
+      <div className="template3-wrapper">
+        <div className={'template-container'} id={id}>
+            <div className={'lp'}>
+                <div className={'lp_personalInfo'}>
+                    <div className={'lp_personalInfo_name'}>{resumeData?.personalInfo?.name}</div>
+                    <div className={'lp_personalInfo_role'}>{resumeData?.personalInfo?.title}</div>
                 </div>
-                <div className={styles['lp_add_info']}>
-                    <div className={styles['lp_add_info_value']}>
+                <div className={'lp_add_info'}>
+                    <div className={'lp_add_info_value'}>
                         <IoMail />
                         {resumeData.personalInfo.email}
                     </div>
-                    <div className={styles['lp_add_info_value']}>
+                    <div className={'lp_add_info_value'}>
                         <FaPhoneAlt/>
                         {resumeData.personalInfo.phone}
                     </div>
-                    <div className={styles['lp_add_info_value']}>
+                    <div className={'lp_add_info_value'}>
                         <FaLinkedin/>
                         <a href={resumeData.personalInfo?.linkedin}>{resumeData.personalInfo?.linkedin}</a>
                     </div>
                 </div>
-                <div className={`${styles['lp_education_div']} ${styles['lp_container']}`}>
-                    <div className={`${styles['lp_education_div_label']} ${styles['lp_container_header']}`}>
+                <div className={`${'lp_education_div'} ${'lp_container'}`}>
+                    <div className={`${'lp_education_div_label'} ${'lp_container_header'}`}>
                         Education
                     </div>
-                    <div className={`${styles['lp_education_div_value']}`}>
+                    <div className={`${'lp_education_div_value'}`}>
                         {
                             resumeData.education?.map((edu:any) => {
                                 return(
-                                    <div className={styles['lp_education_div_value_section']}>
-                                        <div className={styles['lp_education_div_value_section_name']}>{edu?.degree + " - " + edu?.major + ", "+edu?.institution}</div>
-                                        <div className={styles['lp_education_div_value_section_year']}>{edu?.start + " - " + edu?.end}</div>
+                                    <div className={'lp_education_div_value_section'}>
+                                        <div className={'lp_education_div_value_section_name'}>{edu?.degree + " - " + edu?.major + ", "+edu?.institution}</div>
+                                        <div className={'lp_education_div_value_section_year'}>{edu?.start + " - " + edu?.end}</div>
                                     </div>
                                 )
                             })
                         }
                     </div>
                 </div>
-                <div className={`${styles['lp_skill_div']} ${styles['lp_container']}`}>
-                    <div className={`${styles['lp_skill_div_label']} ${styles['lp_container_header']}`}>
+                <div className={`${'lp_skill_div'} ${'lp_container'}`}>
+                    <div className={`${'lp_skill_div_label'} ${'lp_container_header'}`}>
                         Skill
                     </div>
-                    <div className={`${styles['lp_skill_div_value']}`}>
+                    <div className={`${'lp_skill_div_value'}`}>
                         {
                             resumeData?.coreSkills?.map((item:any)=>{
                                 return(
-                                    <div className={styles['lp_skill_div_value_item']}>{item}</div>
+                                    <div className={'lp_skill_div_value_item'}>{item}</div>
                                 )
                             })
                         }
                         {
                             resumeData?.coreSkills?.map((item:any)=>{
                                 return(
-                                    <div className={styles['lp_skill_div_value_item']}>{item}</div>
+                                    <div className={'lp_skill_div_value_item'}>{item}</div>
                                 )
                             })
                         }
                     </div>
                 </div>
             </div>
-            <div className={styles['rp']}>
-                <div className={`${styles['rp_bio_div']} ${styles['rp_container']}`}>
-                    <div className={`${styles['rp_bio_div_label']} ${styles['rp_container_header']}`}>
+            <div className={'rp'}>
+                <div className={`${'rp_bio_div'} ${'rp_container'}`}>
+                    <div className={`${'rp_bio_div_label'} ${'rp_container_header'}`}>
                         PROFILE
                     </div>
-                    <div className={`${styles['rp_bio_div_value']} ${styles['rp_container_content']}`}>
+                    <div className={`${'rp_bio_div_value'} ${'rp_container_content'}`}>
                         {resumeData.personalInfo.bio}
                     </div>
                 </div>
-                <div className={`${styles['rp_experience_div']} ${styles['rp_container']}`}>
-                    <div className={`${styles['rp_experience_div_label']} ${styles['rp_container_header']}`}>
+                <div className={`${'rp_experience_div'} ${'rp_container'}`}>
+                    <div className={`${'rp_experience_div_label'} ${'rp_container_header'}`}>
                         Experience
                     </div>
-                    <div className={`${styles['rp_experience_div_value']} ${styles['rp_container_content']}`}>
+                    <div className={`${'rp_experience_div_value'} ${'rp_container_content'}`}>
                         {
                             resumeData.experience?.map((exp:any) => {
                                 return(
-                                    <div className={styles['rp_experience_div_value_section']}>
-                                        <div className={styles['rp_experience_div_value_section_name']}>{exp?.company + " - " + exp?.role}</div>
-                                        <div className={styles['rp_experience_div_value_section_year']}>{exp?.duration || (exp?.start + " - " + exp.end)}</div>
+                                    <div className={'rp_experience_div_value_section'}>
+                                        <div className={'rp_experience_div_value_section_name'}>{exp?.company + " - " + exp?.role}</div>
+                                        <div className={'rp_experience_div_value_section_year'}>{exp?.duration || (exp?.start + " - " + exp.end)}</div>
                                         <div 
-                                            className={styles['rp_experience_div_value_section_work']}
+                                            className={'rp_experience_div_value_section_work'}
                                             dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(exp.responsibilities) }}
                                         ></div>
                                     </div>
@@ -100,19 +101,19 @@ export const Template3 = (props:any) => {
                         }
                     </div>
                 </div>
-                <div className={`${styles['rp_experience_div']} ${styles['rp_container']}`}>
-                    <div className={`${styles['rp_experience_div_label']} ${styles['rp_container_header']}`}>
+                <div className={`${'rp_experience_div'} ${'rp_container'}`}>
+                    <div className={`${'rp_experience_div_label'} ${'rp_container_header'}`}>
                         Experience
                     </div>
-                    <div className={`${styles['rp_experience_div_value']} ${styles['rp_container_content']}`}>
+                    <div className={`${'rp_experience_div_value'} ${'rp_container_content'}`}>
                         {
                             resumeData.experience?.map((exp:any) => {
                                 return(
-                                    <div className={styles['rp_experience_div_value_section']}>
-                                        <div className={styles['rp_experience_div_value_section_name']}>{exp?.company + " - " + exp?.role}</div>
-                                        <div className={styles['rp_experience_div_value_section_year']}>{exp?.duration || (exp?.start + " - " + exp.end)}</div>
+                                    <div className={'rp_experience_div_value_section'}>
+                                        <div className={'rp_experience_div_value_section_name'}>{exp?.company + " - " + exp?.role}</div>
+                                        <div className={'rp_experience_div_value_section_year'}>{exp?.duration || (exp?.start + " - " + exp.end)}</div>
                                         <div 
-                                            className={styles['rp_experience_div_value_section_work']}
+                                            className={'rp_experience_div_value_section_work'}
                                             dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(exp.responsibilities) }}
                                         ></div>
                                     </div>
@@ -123,5 +124,6 @@ export const Template3 = (props:any) => {
                 </div>
             </div>
         </div>
+      </div>
     );
 };
