fdac23c9973eac6d00b160ce70ee3d8b9a4eff57
Sameeksha Dubey <sameekshadubey@Sameekshas-MacBook-Air.local>
2024-10-17 21:26:52 +0530

    about us connected and checkout page started

---
+++ apps/docs/app/(pages)/select-templates/checkout/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import CheckoutPage from "../../../../components/Checkout";
+
+const Checkout = () => {
+  return <CheckoutPage />;
+};
+
+export default Checkout;
