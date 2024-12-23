7768b3a65c075bcd99a5c76c5e35e6ae5ac89e43
Yash Singh <myemail>
2024-12-24 02:27:02 +0530

    ci/cd new

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,9 +3,7 @@ import LandingPage from "../components/Landing/Landing";
 export default async function Page() {
   return (
     <main>
-      {/* <Frontpage />  */}
       <LandingPage />
-      {/* not logged in */}
     </main>
   );
 }
