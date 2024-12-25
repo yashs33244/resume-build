67dfda4aed5ca534bdd43266f5b23091a9f41d81
Sameeksha Dubey <sameekshadubey@Sameekshas-MacBook-Air.local>
2024-12-25 15:27:29 +0530

    assets updated

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -72,13 +72,13 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico" },
-      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.png", sizes: "96x96", type: "image/png" },
+      { url: "/favicon.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.png", sizes: "16x16", type: "image/png" },
     ],
     shortcut: ["/favicon.ico"],
     apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+      { url: "/favicon.png", sizes: "180x180", type: "image/png" },
     ],
     other: [
       {
